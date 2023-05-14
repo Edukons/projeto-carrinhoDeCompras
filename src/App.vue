0de5bb90e485cac024ee6f4553d1e7f3bce5a7ec
Edukons <eduardo.kons03@gmail.com>
2023-05-14 12:52:01 -0300

    inicio css

--- src/App.vue
+++ src/App.vue
@@ -2,77 +2,77 @@
 import { ref } from 'vue'
 
 const item = ref([
-{ 
-        id: 1,
-        nome: 'Caderno 10 matérias',
-        preco: 17.50,
-        quantidade: 0,
-    },
-    {
-        id: 2,
-        nome: 'Caderno 1 matéria',
-        preco: 9.90,
-        quantidade: 0,
-    },
-    {
-        id: 3,
-        nome: 'Lápis de cor Faber-Castell',
-        preco: 27.90,
-        quantidade: 0,
-    },
-    {
-        id: 4,
-        nome: 'EcoLápis SuperSoft Faber-Castell',
-        preco: 39.99,
-        quantidade: 0,
-    },
-    {
-        id: 5,
-        nome: 'Kit escolar Faber-Castell',
-        preco: 28.90,
-        quantidade: 0,
-    },
-    {
-        id: 6,
-        nome: 'Kit caneta esferográfica Faber-Castell',
-        preco: 7.90,
-        quantidade: 0,
-    },
-    {
-        id: 7,
-        nome: 'Coleção Lettering Básico Faber-Castell',
-        preco: 55.90,
-        quantidade: 0,
-    },
-    {
-        id: 8,
-        nome: 'Marca Texto Textliner Pastel 46 Faber-Castell',
-        preco: 59.90,
-        quantidade: 0,
-    },
-    {
-        id: 9,
-        nome: 'Lápis Grafite EcoLápis Castell 9000 2B/6B/8B Sextavado Faber-Castell',
-        preco: 10.60,
-        quantidade: 0,
-    },
-    {
-        id: 10,
-        nome: 'Apontador com depósito Pôster Estampas Sortidas, Faber-Castell',
-        preco: 6.90,
-        quantidade: 0,
-    }
+  {
+    id: 1,
+    nome: 'Caderno 10 matérias',
+    preco: 17.50,
+    quantidade: 0,
+  },
+  {
+    id: 2,
+    nome: 'Caderno 1 matéria',
+    preco: 9.90,
+    quantidade: 0,
+  },
+  {
+    id: 3,
+    nome: 'Lápis de cor Faber-Castell',
+    preco: 27.90,
+    quantidade: 0,
+  },
+  {
+    id: 4,
+    nome: 'EcoLápis SuperSoft Faber-Castell',
+    preco: 39.99,
+    quantidade: 0,
+  },
+  {
+    id: 5,
+    nome: 'Kit escolar Faber-Castell',
+    preco: 28.90,
+    quantidade: 0,
+  },
+  {
+    id: 6,
+    nome: 'Kit caneta esferográfica Faber-Castell',
+    preco: 7.90,
+    quantidade: 0,
+  },
+  {
+    id: 7,
+    nome: 'Coleção Lettering Básico Faber-Castell',
+    preco: 55.90,
+    quantidade: 0,
+  },
+  {
+    id: 8,
+    nome: 'Marca Texto Textliner Pastel 46 Faber-Castell',
+    preco: 59.90,
+    quantidade: 0,
+  },
+  {
+    id: 9,
+    nome: 'Lápis Grafite EcoLápis Castell 9000 2B/6B/8B Sextavado Faber-Castell',
+    preco: 10.60,
+    quantidade: 0,
+  },
+  {
+    id: 10,
+    nome: 'Apontador com depósito Pôster Estampas Sortidas, Faber-Castell',
+    preco: 6.90,
+    quantidade: 0,
+  }
 ])
 
 function incrementar(index) {
-    item.value[index].quantidade++
-  }
-  function descrementar(index) {
-    item.value[index].quantidade--
-  }
-  let valortotal = ref(0)
+  item.value[index].quantidade++
+}
+function descrementar(index) {
+  item.value[index].quantidade--
+}
+let valortotal = ref(0)
 
-  let carrinho = ref([])
+let carrinho = ref([])
 function addCarrinho(item) {
   carrinho.value.push({
     codigo: item.id,
@@ -84,9 +84,9 @@ function addCarrinho(item) {
   totalValor()
   item.quantidade = 1
 }
-function totalValor(){
+function totalValor() {
   for (let contador = 0; contador < carrinho.value.length; contador++) {
-    valortotal.value = valortotal.value + carrinho.value[contador].totalItem  
+    valortotal.value = valortotal.value + carrinho.value[contador].totalItem
   }
 }
 function limpaCarrinho() {
@@ -99,51 +99,87 @@ function remover(index) {
 </script>
 
 <template>
-  <div class="row">
+  <div class="col align-items-center">
     <ul>
-      <li class="lista" v-for="(item, index) in item" :key="item.id">
+      <li class="produtos col" v-for="(item, index) in item" :key="item.id">
         <p>Item: {{ item.nome }}</p>
         <p>Preco: {{ item.preco }}</p>
-        <p>ID: {{ item.id }}</p>        
         <p>quantidade: {{ item.quantidade }}</p>
-        <button @click="incrementar(index)">+</button>
-        <button @click="descrementar(index)">-</button>
-        <button @click="addCarrinho(item)">Adicionar</button>
+        <button class="produtos:hover aad " @click="incrementar(index)">+</button>
+        <button class="produtos:hover aad" @click="descrementar(index)">-</button>
+        <button class="produtos:hover aad" @click="addCarrinho(item)">Adicionar</button>
       </li>
     </ul>
     <hr>
-    <li v-for="item in carrinhos">
-        <p>Item: {{ item.name }}</p>
-
-        <p>Preco: {{ item.price }}</p>
-        <p>ID: {{ item.id }}</p>
-    </li>
   </div>
 
   <ul>
     <div class="">
-    <h1>Carrinho</h1> 
-    <ul>
-      <li v-for="(item, index) in carrinho" :key="index">{{ item.nome }} 
-      <p> valor: {{(item.preco) }} Preço</p>
-    <p>Quant: {{ item.quantidade }}</p>
-    <button id="remove" @click="remover(index)">remover item</button>  
-    <p class="letraTotal">Preço total: {{ (item.totalItem) }}</p>
-    
-   
-      </li>
-    </ul>
-    <button @click="limpaCarrinho()">Limpar carrinho</button>
-  </div>
-</ul>
+      <h1>Carrinho</h1>
+      <ul>
+        <li v-for="(item, index) in carrinho" :key="index">{{ item.nome }}
+          <p> valor: {{ (item.preco) }} Preço</p>
+          <p>Quant: {{ item.quantidade }}</p>
+          <button id="remove" @click="remover(index)">remover item</button>
+          <p class="letraTotal">Preço total: {{ (item.totalItem) }}</p>
+
+
+        </li>
+      </ul>
+      <button @click="limpaCarrinho()">Limpar carrinho</button>
+    </div>
+  </ul>
 </template>
 
 <style scoped>
-
-
-ul{
-  display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: 5fr 5fr;
-}
+  .produtos {
+    margin: 20px;
+    width: calc(33.33% - 40px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    box-shadow: 0px 2px 4px rgba(0,0,0,0.2);
+    border-radius: 5px;
+    transition: transform 0.2s ease-in-out;
+  }
+  .produtos:hover {
+    transform: translateY(-5px);
+  }
+  .produtos p {
+    margin: 0;
+    text-align: center;
+  }
+  .produtos p:nth-of-type(1) {
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  .produtos p:nth-of-type(2) {
+    font-size: 1rem;
+    margin-bottom: 10px;
+  }
+  .produtos p:nth-of-type(3) {
+    font-size: 0.8rem;
+    margin-bottom: 10px;
+  }
+  .produtos button {
+    height: 30px;
+    width: 100%;
+    border-radius: 5px;
+    background-color: #4CAF50;
+    color: #fff;
+    font-size: 0.9rem;
+    cursor: pointer;
+    border: none;
+    margin-top: auto;
+  }
+  .produtos button:hover {
+    background-color: #3e8e41;
+  }
+  .aad{
+    width: 200px;
+  }
 </style>
